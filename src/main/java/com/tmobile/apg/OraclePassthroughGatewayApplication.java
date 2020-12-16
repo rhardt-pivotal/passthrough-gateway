df7bb86e653cf17df449e2aa39240796cbf693b6
Rob Hardt <rhardt@pivotal.io>
2020-12-15 21:12:00 -0800

    first commit

---
+++ src/main/java/com/tmobile/apg/OraclePassthroughGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.tmobile.apg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OraclePassthroughGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OraclePassthroughGatewayApplication.class, args);
+	}
+
+}
